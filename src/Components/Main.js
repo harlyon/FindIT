c50dba288f143408a524d09a01c5f077213ddcc0
Harrison <harrisonekpobimi@gmail.com>
2018-11-13 08:08:59 +0200

    changes

---
+++ src/Components/Main.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Main = () => {
+    return (
+        <div>
+            <div className="jumbotron jumbotron-fluid">
+                <div className="container">
+                    <h1>FindIT - Information Retrival</h1>
+                    <p className="lead">Search for places, venues search and more .</p>
+                    <a className="btn btn-primary btn-lg" href="/" role="button">Search</a>
+                </div>
+        </div>
+        </div>
+    );
+};
+
+export default Main;
